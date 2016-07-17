f4aaf2db0c8f59f089257f90b8fb1ecc7eb526ad
William Huster <whusterj@gmail.com>
2016-07-17 13:08:54 -0400

    Add Bill object to __init__.py list of exports

--- quickbooks/__init__.py
+++ quickbooks/__init__.py
@@ -2,6 +2,7 @@ from .client import QuickBooks
 
 from .objects.account import Account
 from .objects.base import Ref
+from .objects.bill import Bill
 from .objects.customer import Customer
 from .objects.invoice import (DeliveryInfo, Invoice, )
 from .objects.payment import (PaymentLine, Payment, )
