fb7fa9e5547bb587e747046c3dedaaa167eed8b5
William Huster <whusterj@gmail.com>
2016-07-17 13:43:55 -0400

    Add Bill expense line details to quickbooks.__init__

--- quickbooks/__init__.py
+++ quickbooks/__init__.py
@@ -2,7 +2,10 @@ from .client import QuickBooks
 
 from .objects.account import Account
 from .objects.base import Ref
-from .objects.bill import (Bill, BillLine, )
+from .objects.bill import (
+    Bill, BillLine, AccountBasedExpenseLineDetail,
+    ItemBasedExpenseLineDetail
+)
 from .objects.customer import Customer
 from .objects.invoice import (DeliveryInfo, Invoice, )
 from .objects.payment import (PaymentLine, Payment, )
